813376d21c27f14a242fb4e85bfca92e2e4b9463
Jisang you <jisangman89@gmail.com>
2017-10-10 00:06:55 +0900

    first commit graphicnanimation

---
+++ src/main/java/orgs/androidtown/graphanim/MainActivity.java
@@ -0,0 +1,41 @@
+package orgs.androidtown.graphanim;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ProgressBar;
+
+public class MainActivity extends AppCompatActivity {
+    ProgressBar progressBar,progressBar2, progressBar3;
+    Animation grow;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar2 = (ProgressBar) findViewById(R.id.progressBar2);
+        progressBar3 = (ProgressBar) findViewById(R.id.progressBar3);
+
+        grow = AnimationUtils.loadAnimation(this, R.anim.grow);
+        progressBar.setAnimation(grow);
+        progressBar2.setAnimation(grow);
+        progressBar3.setAnimation(grow);
+
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if(hasFocus){
+            grow.start();
+        }else{
+            grow.reset();
+        }
+    }
+}
